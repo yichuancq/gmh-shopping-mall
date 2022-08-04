8e178092570a51888e005c578a014fbed69111e5
yichuancq <yichuancq@hotmail.com>
2022-08-04 15:10:09 +0800

    添加分页查询逻辑

--- gmh-order-service/src/main/java/com/gmh/order/service/OrderService.java
+++ gmh-order-service/src/main/java/com/gmh/order/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.gmh.order.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.gmh.mail.common.response.ResultData;
 import com.gmh.order.domain.OrderEntity;
+import com.gmh.order.vo.QueryOrderVo;
 
 import java.math.BigInteger;
 import java.util.List;
@@ -30,4 +32,10 @@ public interface OrderService extends IService<OrderEntity> {
      * @throws Exception
      */
     boolean deleteById(BigInteger id) throws Exception;
+
+    /**
+     * @param queryOrderVo
+     * @return
+     */
+    ResultData<?> pagerOrderList(QueryOrderVo queryOrderVo) throws Exception;
 }
--- gmh-order-service/src/main/java/com/gmh/order/service/impl/OrderServiceImpl.java
+++ gmh-order-service/src/main/java/com/gmh/order/service/impl/OrderServiceImpl.java
@@ -1,11 +1,17 @@
 package com.gmh.order.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.IdWorker;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gmh.mail.common.exception.ResultCode;
+import com.gmh.mail.common.response.ResultData;
 import com.gmh.order.domain.OrderEntity;
 import com.gmh.order.mapper.OrderMapper;
 import com.gmh.order.service.OrderService;
+import com.gmh.order.utils.PageUtils;
+import com.gmh.order.vo.QueryOrderVo;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -79,5 +85,45 @@ public class OrderServiceImpl extends ServiceImpl<OrderMapper, OrderEntity> impl
             return true;
         }
         return false;
+    }
+
+    /**
+     * 分页查询订单
+     *
+     * @param queryOrderVo
+     * @return
+     */
+
+    @Override
+    public ResultData<?> pagerOrderList(QueryOrderVo queryOrderVo) throws Exception {
+        if (queryOrderVo == null) {
+            throw new Exception("查询参数为空");
+        }
+        IPage<OrderEntity> pageParams = new Page<>();
+        //当前页
+        pageParams.setCurrent(queryOrderVo.getPageNumber());
+        //每页条数
+        pageParams.setSize(queryOrderVo.getPageSize());
+        //分页查询
+        QueryWrapper<OrderEntity> queryWrapper = new QueryWrapper<>();
+        //订单号
+        if (queryOrderVo.getOrderNumber() != null) {
+            queryWrapper.eq("order_number", queryOrderVo.getOrderNumber());
+        }
+        //购买用户昵称
+        if (queryOrderVo.getNickName() != null) {
+            queryWrapper.eq("user_nickname", queryOrderVo.getNickName());
+        }
+        //开始时间和结束时间
+        if (queryOrderVo.getStartTime() != null && queryOrderVo.getEndTime() != null) {
+            if (!queryOrderVo.getStartTime().isEmpty() && !queryOrderVo.getEndTime().isEmpty()) {
+                queryWrapper.between("create_time", queryOrderVo.getStartTime(), queryOrderVo.getEndTime());
+            }
+        }
+        queryWrapper.orderByDesc("create_time");
+        //
+        IPage<OrderEntity> orderPage = this.getBaseMapper().selectPage(pageParams, queryWrapper);
+        return new ResultData(ResultCode.SUCCESS, new PageUtils(orderPage.getRecords(), (int) orderPage.getTotal(),
+                (int) pageParams.getSize(), (int) pageParams.getCurrent()));
     }
 }
